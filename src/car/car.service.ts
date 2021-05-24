c334a65b9ee2adb477d724859a73fb99abf89d58
IBMADMIN <IBMADMIN@LAPTOP-5DPV2C32.ibmbarclays.com>
2021-05-24 07:54:02 +0530

    changed srvices and removed Promises

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { resolve } from 'path';
+// import { resolve } from 'path';
 import { CARS } from './cars.mock';
 
 @Injectable()
@@ -15,42 +15,36 @@ export class CarService {
     return this.cars.push(car);
   }
   //get car by id
-  public async getCarbyId(id: number): Promise<any> {
+  public async getCarbyId(id: number) {
     const carId = Number(id);
-    return new Promise((resolve) => {
-      const car = this.cars.find((car) => car.id === carId);
-      if (!car) {
-        throw new HttpException('not found', 404);
-      }
-      return resolve(car);
-    });
+    const car = this.cars.find((car) => car.id === carId);
+    if (!car) {
+      throw new HttpException('not found', 404);
+    }
+    return car;
   }
   // Delete car by Id
-  public async deleteCarById(id: number): Promise<any> {
+  public async deleteCarById(id: number) {
     const carId = Number(id);
     const carIndex = this.cars.findIndex((car) => car.id === carId);
-    return new Promise((resolve) => {
-      if (carIndex === -1) {
-        throw new HttpException('not found', 404);
-      }
-      this.cars.splice(carIndex, 1);
-      return resolve(this.cars);
-    });
+    if (carIndex === -1) {
+      throw new HttpException('not found', 404);
+    }
+    this.cars.splice(carIndex, 1);
+    return this.cars;
   }
   // Put car by ID
   public async putCarById(
     id: number,
     propertyName: string,
     propertyValue: string,
-  ): Promise<any> {
+  ) {
     const carId = Number(id);
     const carIndex = this.cars.findIndex((car) => car.id === carId);
-    return new Promise((resolve) => {
-      if (carIndex === -1) {
-        throw new HttpException('not found', 404);
-      }
-      this.cars[carIndex][propertyName] = propertyValue;
-      return resolve(this.cars);
-    });
+    if (carIndex === -1) {
+      throw new HttpException('not found', 404);
+    }
+    this.cars[carIndex][propertyName] = propertyValue;
+    return this.cars;
   }
 }
